6a0212ce0e0e51eca5941e9945fe376821cdf019
lucerion <alexander.skachko@gmail.com>
2019-04-15 19:09:49 +0300

    Add Core and Parser applications

---
+++ apps/reedly_core/lib/application.ex
@@ -0,0 +1,16 @@
+defmodule Reedly.Core.Application do
+  @moduledoc "Reedly Core application"
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Reedly.Core.Repo, [])
+    ]
+    opts = [strategy: :one_for_one, name: Reedly.Core.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+end
