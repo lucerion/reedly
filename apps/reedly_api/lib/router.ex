f3e9a0b5f59fcb586dad666a1c4670bfad053a12
lucerion <alexander.skachko@gmail.com>
2019-04-23 04:09:01 +0300

    Fix Absinthe routing
    plug :match and :dispatch should be after Absinthe plug

--- apps/reedly_api/lib/router.ex
+++ apps/reedly_api/lib/router.ex
@@ -3,8 +3,12 @@ defmodule Reedly.API.Router do
 
   use Plug.Router
 
-  plug(:match)
-  plug(:dispatch)
+  alias Plug.Conn.Status
+
+  @init_opts [
+    schema: Reedly.API.Schema,
+    json_codec: Jason
+  ]
 
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
@@ -14,15 +18,23 @@ defmodule Reedly.API.Router do
 
   forward("/api",
     to: Absinthe.Plug,
-    init_opts: [schema: Reedly.API.Schema, json_codec: Jason]
+    init_opts: @init_opts
   )
 
-  forward("/graphiql",
-    to: Absinthe.Plug.GraphiQL,
-    init_opts: [schema: Reedly.API.Schema, json_codec: Jason]
-  )
+  if Mix.env() == :dev do
+    forward("/graphiql",
+      to: Absinthe.Plug.GraphiQL,
+      init_opts: @init_opts
+    )
+  end
+
+  plug(:match)
+  plug(:dispatch)
 
   match _ do
-    send_resp(conn, 404, "Bad Request")
+    status_code = Status.code(:not_found)
+    reason_phrase = Status.reason_phrase(status_code)
+
+    send_resp(conn, status_code, reason_phrase)
   end
 end
