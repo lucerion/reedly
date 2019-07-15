87947a7a96f811a6ac945683304cbf4b481445af
lucerion <alexander.skachko@gmail.com>
2019-07-15 14:54:13 +0300

    Add more timestamp formats to DateTimeHelper

--- apps/reedly_core/lib/helpers/date_time_helper.ex
+++ apps/reedly_core/lib/helpers/date_time_helper.ex
@@ -1,13 +1,39 @@
 defmodule Reedly.Core.Helpers.DateTimeHelper do
-  @moduledoc "Dates helper functions"
+  @moduledoc """
+  Dates helper functions
 
-  @default_format "%a, %d %b %Y %H:%M:%S %z"
+  Timestamp formats
 
-  @spec parse(String.t(), String.t()) :: NaiveDateTime.t() | nil
-  def parse(date_time_string, format \\ @default_format) do
-    case Timex.parse(date_time_string, format, :strftime) do
+  * `{RFC822}`   - `Mon, 05 Jun 14 23:20:59 UT`
+  * `{RFC822z}`  - `Mon, 05 Jun 14 23:20:59 Z`
+  * `{RFC1123}`  - `Tue, 05 Mar 2013 23:25:19 +0200`
+  * `{RFC1123z}` - `Tue, 05 Mar 2013 23:25:19 Z`
+  * `{RFC3339}`  - `2013-03-05T23:25:19+02:00`
+  * `{RFC3339z}` - `2013-03-05T23:25:19Z`
+  * `{ANSIC}`    - `Tue Mar 5 23:25:19 2013`
+  * `{UNIX}`     - `Tue Mar 5 23:25:19 PST 2013`
+  """
+
+  @timestamp_formats ~w[
+    {RFC822}
+    {RFC822z}
+    {RFC1123}
+    {RFC1123z}
+    {RFC3339}
+    {RFC3339z}
+    {ANSIC}
+    {UNIX}
+  ]
+
+  @spec parse(String.t() | nil, list(String.t()) | String.t()) :: NaiveDateTime.t() | nil
+  def parse(nil), do: nil
+  def parse(timestamp), do: parse(timestamp, @timestamp_formats)
+  def parse(timestamp, []), do: nil
+
+  def parse(timestamp, [format | formats]) do
+    case Timex.parse(timestamp, format) do
       {:ok, date_time} -> DateTime.to_naive(date_time)
-      _ -> nil
+      _ -> parse(timestamp, formats)
     end
   end
 end
--- apps/reedly_core/test/helpers/date_time_helper_test.exs
+++ apps/reedly_core/test/helpers/date_time_helper_test.exs
@@ -4,12 +4,16 @@ defmodule Reedly.Core.Test.DateTimeHelperTest do
   alias Reedly.Core.Helpers.DateTimeHelper
 
   describe "parse/1" do
-    test "returns a parsed date time when date time string is valid" do
+    test "returns a parsed date time" do
       assert DateTimeHelper.parse("Mon, 10 Jan 2011 12:13:14 +0000") == ~N[2011-01-10 12:13:14]
     end
 
-    test "returns nil when date time string is not valid" do
-      assert DateTimeHelper.parse("2011-01-10 12:13:14") == nil
+    test "returns nil when timestamp is not valid" do
+      assert DateTimeHelper.parse("Mon 10 Jan 2011 12:13:14 +0000") == nil
+    end
+
+    test "returns nil when timestamp is nil" do
+      assert DateTimeHelper.parse(nil) == nil
     end
   end
 end
